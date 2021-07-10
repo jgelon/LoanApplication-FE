7e5c70f739e6b627e07e36613497081d7d737cc4
Jurn van Mieghem <j.gelon@gmail.com>
2021-07-10 16:47:48 +0200

    Radiogroup

--- src/app/components/request-a-loan/request-a-loan.component.ts
+++ src/app/components/request-a-loan/request-a-loan.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { LoanType } from 'src/app/model/loantype';
+import { LoanTypesService } from 'src/app/service/loantypes.service';
 
 @Component({
   selector: 'app-request-a-loan',
@@ -8,20 +10,25 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class RequestALoanComponent implements OnInit {
 
+  loanTypes: LoanType[];
   isLinear = true;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder, private _loanTypesService: LoanTypesService) {}
   
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
+      loanType: ['', Validators.required],
       description: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       amount: ['', Validators.required],
       stock: ['', Validators.required]
+    });
+    this._loanTypesService.findAll().subscribe(data => {
+      this.loanTypes = data;
     });
   }
   

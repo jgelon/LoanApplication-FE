67a510a2f0bce6095232beafcf7857dea0044584
Jurn van Mieghem <j.gelon@gmail.com>
2021-07-10 15:35:24 +0200

    LoanRequest updated

--- src/app/request-loan/request-loan.component.ts
+++ src/app/request-loan/request-loan.component.ts
@@ -10,16 +10,23 @@ import { LoanTypesService } from '../service/loantypes.service';
 })
 export class RequestLoanComponent implements OnInit {
 
-   loanTypes: LoanType[];
-
-   model = new LoanRequest();
+  loanTypes: LoanType[];
+  model = new LoanRequest();
+  submitted = false;
 
   constructor(private loanTypesService: LoanTypesService) { }
 
   ngOnInit(): void {
      this.loanTypesService.findAll().subscribe(data => {
        this.loanTypes = data;
+       this.model.loanType = this.loanTypes[0];
      });
+  }
+
+  onSubmit() { this.submitted = true; }
+
+  newLoanRequest() {
+    this.model = new LoanRequest();
   }
 
 }

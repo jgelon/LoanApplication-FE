e7cde2df691acca0d99d9a811a57185453740d77
JGelon <j.gelon@gmail.com>
2025-06-12 09:32:43 +0200

    Add gravity collector

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://loanapplication-be.gmie.nl'
+  api: 'https://loanapplication-be.gmie.nl',
+  gravityAuthKey: '3cbecc58-fe9c-4573-a1d0-b48b07d81de4',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   api: 'https://loanapplication-be.gmie.nl',
   //api: 'http://localhost:8080'
+  gravityAuthKey: '3cbecc58-fe9c-4573-a1d0-b48b07d81de4',
 };
 
 /*
